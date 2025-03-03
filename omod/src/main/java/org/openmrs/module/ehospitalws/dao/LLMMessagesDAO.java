352182b38532b6b3ac1073fc7edec5533b7564ee
Your Name <michaelndula@gmail.com>
2025-03-03 13:07:12 +0300

    Get latest message for patient and update the db after sending to the message Patient

--- omod/src/main/java/org/openmrs/module/ehospitalws/dao/LLMMessagesDAO.java
+++ omod/src/main/java/org/openmrs/module/ehospitalws/dao/LLMMessagesDAO.java
@@ -2,6 +2,7 @@ package org.openmrs.module.ehospitalws.dao;
 
 import org.openmrs.module.ehospitalws.model.LLMMessages;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 public interface LLMMessagesDAO {
@@ -11,4 +12,8 @@ public interface LLMMessagesDAO {
 	LLMMessages getMessageById(Long id);
 	
 	List<LLMMessages> getMessagesByPatientUuid(String patientUuid);
+	
+	void updateMessageStatus(Long id, String status, Timestamp sentAt);
+	
+	LLMMessages getLatestMessageByPatientUuid(String patientUuid);
 }
